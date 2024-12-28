82a0c14f54a9560d71c7d2bec85717538b707b75
rayan-saleh <86924567+rayan-saleh@users.noreply.github.com>
2024-12-28 20:42:11 +0000

    change dates location

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,13 +8,14 @@ const BlogPost = ({ post }) => {
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
       <article key={post.id} className="">
-        <header className="flex mb-2 md:flex-row items-baseline">
-          <time className="flex-shrink-0 mr-2 text-gray-600 dark:text-gray-400">
-            <FormattedDate date={post.date} />
-          </time>
+        <header className="flex mb-2 md:flex-row items-baseline justify-between">
+          
           <h2 className="text-lg cursor-pointer text-black dark:text-gray-100">
             {post.title}
           </h2>
+          <time className="flex-shrink-0 mr-2 text-gray-600 dark:text-gray-400">
+            <FormattedDate date={post.date} />
+          </time>
         </header>
         {/* <main>
           <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
